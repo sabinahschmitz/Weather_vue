093010de2184f664f7b8ef6402bf6c298cb5d027
Sabina Schmitz <sabinahschmitz@gmail.com>
2022-06-25 19:53:22 +0200

    Added images and basic CSS

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,26 @@ export default {
 
 <style>
 
+*{
+  /* margin zero brings the search box all the way to the edge */
+  margin: 0;
+/* padding zero takes all the spacing away from the search box */
+  padding: 0;
+  /* tells the browser to account for any border and padding in the values you specify for an element's width and height. */
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
+/* main component */
+#app {
+  /* background image */
+  background-image: url('./assets/winter.jpg');
+  /* Scales the image (while preserving its ratio) to the smallest possible size to fill the container  */
+  background-size: cover;
+  /*  sets the initial position for each background image. */
+  background-position: bottom;
+}
 </style>
