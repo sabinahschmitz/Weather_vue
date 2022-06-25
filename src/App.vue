26a46412d129ff334e093270d936efc96c8ca68f
Sabina Schmitz <sabinahschmitz@gmail.com>
2022-06-25 19:46:39 +0200

    Added search box

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-
+<main>
+  <div class="search-box">
+    <input type="text" class="search-bar" placeholder="search..."/>
+  </div>
+</main>
   </div>
 
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return { //object
+      api_key: '896bcef6e113845505149836504d38ea'
+    }
+  }
   
 }
 </script>
