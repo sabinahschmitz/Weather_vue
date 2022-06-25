2a3e73113d7f882ccc3098386d8d2c0ded45a36d
Sabina Schmitz <sabinahschmitz@gmail.com>
2022-06-25 22:14:27 +0200

    more stuff

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
 <main>
+ <!-- v-model: creates a two way binding on a form input element or compnents -- binds it to the data elemt query -->
+<!-- at keypress it's going to fetch the weather (function within methods) -->
   <div class="search-box">
     <input 
     type="text" 
     class="search-bar" 
-    placeholder="search..."/>
+    placeholder="search..."
+    v-model="query"
+    @keypress="fetchWeather" 
+    />
   </div>
 
-  <div class="weather-wrap">
+  <div class="weather-wrap" v-if ="typeof weather.main != 'undefined'">
     <div class="location-box">
-      <div class="location">Dormagen, DE</div>
+      <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
       <div class="date">Saturday 25 June 2022</div>
     </div>
 
     <div class="weather-box">
-      <div class="temp">9°</div>
-      <div class="weather">Rain</div>
+      <div class="temp"> {{Math.round(weather.main.temp)}}</div>
+      <div class="weather">{{weather.weather[0].main}}</div>
     </div>
   </div>
 </main>
@@ -29,8 +34,41 @@ export default {
   name: 'app',
   data () {
     return { //object
-      api_key: '896bcef6e113845505149836504d38ea',
-      url_base:'https://api.openweathermap.org/data/2.5/'
+      api_key: '34f6147fb63fca2fd41a930bdc18e9d6',
+      url_base:'https://api.openweathermap.org/data/2.5/',
+
+      // to bind the search box we need a query
+      query: '',
+
+      // somewhere to store the weather from the data we get back
+      weather: {}
+    }
+  },
+  // methods - somewhere to actually store the information
+  methods: {
+    fetchWeather (e) {
+      // we dont want this function to be activated when "any key" is pressed. only when the enter key is pressed
+      if (e.key =="Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`) //template string - q stands for query
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month= months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year};
     }
   }
   
